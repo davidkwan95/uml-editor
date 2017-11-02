2c4a4e598528dd73a536c697d1f593c64d60f6ad
David Kwan <davidkwan95@gmail.com>
2017-11-03 00:09:11 +0700

    add cleanwebpack plugin when building

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,17 +1,24 @@
 const path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+const rootPath = path.resolve(__dirname, '../');
+const outputPath = path.resolve(__dirname, '../dist');
 
 module.exports = {
   entry: './js/index.js',
   output: {
-    path: path.resolve(__dirname, '../dist'),
+    path: outputPath,
     filename: '[hash].bundle.js'
   },
   plugins: [
+    new CleanWebpackPlugin([outputPath], {
+      root: rootPath,
+    }),
     new HtmlWebpackPlugin({
       template: 'index.template.html',
       filename: '../index.html'
-    })
+    }),
   ],
   module: {
     rules: [
