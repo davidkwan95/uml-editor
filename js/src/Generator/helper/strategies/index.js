b34cbc50d7250ec1557533eafc4010369e2edbd3
David Kwan <davidkwan95@gmail.com>
2017-11-04 21:36:02 +0700

    Transform/Refactor Handler and Strategies into ES6

--- js/src/Generator/helper/strategies/index.js
+++ js/src/Generator/helper/strategies/index.js
@@ -1,45 +1,12 @@
-var Handler = require('../Handler');
+import ClassOrInterfaceStrategy from './ClassOrInterfaceStrategy';
+import AttributeStrategy from './AttributeStrategy';
+import MethodStrategy from './MethodStrategy';
+import GeneralizationStrategy from './GeneralizationStrategy';
 
-var ClassOrInterfaceStrategy = function () {};
-ClassOrInterfaceStrategy.prototype = new Handler();
-ClassOrInterfaceStrategy.prototype.handleRequest = function (parser, mxCell, componentName) {
-  if (componentName === 'class' || componentName === 'interface') {
-    return parser.generateAndPushClassOrInterfaceElement(mxCell, componentName);
-  }
-  return this.next.handleRequest(parser, mxCell, componentName);
+export {
+  ClassOrInterfaceStrategy,
+  AttributeStrategy,
+  MethodStrategy,
+  GeneralizationStrategy,
 };
 
-var AttributeStrategy = function () {};
-AttributeStrategy.prototype = new Handler();
-AttributeStrategy.prototype.handleRequest = function (parser, mxCell, componentName) {
-  if (componentName === 'attribute') {
-    return parser.pushAttribute(mxCell);
-  }
-  return this.next.handleRequest(parser, mxCell, componentName);
-};
-
-var MethodStrategy = function () {};
-MethodStrategy.prototype = new Handler();
-MethodStrategy.prototype.handleRequest = function (parser, mxCell, componentName) {
-  if (componentName === 'method') {
-    return parser.pushMethod(mxCell);
-  }
-  return this.next.handleRequest(parser, mxCell, componentName);
-};
-
-var GeneralizationStrategy = function () {};
-GeneralizationStrategy.prototype = new Handler();
-GeneralizationStrategy.prototype.handleRequest = function (parser, mxCell, componentName) {
-  if (componentName === 'generalization') {
-    return parser.pushGeneralization(mxCell);
-  }
-  return this.next.handleRequest(parser, mxCell, componentName);
-};
-
-var exports = {};
-exports.ClassOrInterfaceStrategy = ClassOrInterfaceStrategy;
-exports.AttributeStrategy = AttributeStrategy;
-exports.MethodStrategy = MethodStrategy;
-exports.GeneralizationStrategy = GeneralizationStrategy;
-
-module.exports = exports;
